511c12a9d3ffa8e58352f63b485ef0d1afcdb785
JoMI103 <a21078@alunos.ipca.pt>
2026-04-29 15:40:20 +0100

    Create contact page and setup layout with it

---
+++ Pages/Contacts/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using ContactManager.Services;
+
+namespace ContactManager.Pages.Contacts
+{
+    public class IndexModel(ContactService contactService) : PageModel
+    {
+        private readonly ContactService _contactService = contactService;
+
+        public List<Contact> Contacts { get; set; } = [];
+        public int NumContacts { get; set; } = 0;
+
+        public void OnGet()
+        {
+            Contacts = [.. _contactService.GetCurrentContacts().Where(c => !c.IsDeleted)];
+            NumContacts = Contacts.Count;
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _contactService.SoftDelete(id);
+            return RedirectToPage();
+        }
+    }
+}
