bffccd83d0a48fc6268412daa766cb07d3647f71
JoMI103 <a21078@alunos.ipca.pt>
2026-04-29 15:32:46 +0100

    Setup Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,34 @@
+using ContactManager.Data;
+using ContactManager.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MariaDB"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MariaDB"))
+    ));
+
+builder.Services.AddScoped<ContactService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
+app.MapRazorPages();
 
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
